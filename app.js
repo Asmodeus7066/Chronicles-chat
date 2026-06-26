596341cf741b1d7e6978b4539bf7f942f846a5ac
Asmodeus7066 <139057320+Asmodeus7066@users.noreply.github.com>
2026-06-26 13:28:21 +1000

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZ
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 /* ---------------- STATE ---------------- */
-
 let currentUser = "";
 let currentAvatar = "";
 
